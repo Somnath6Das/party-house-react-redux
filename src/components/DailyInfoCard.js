14dd3c69fb45748440a2b3a982703d4cdc580814
Somnath6Das <98904596+Somnath6Das@users.noreply.github.com>
2022-09-17 23:32:28 +0530

    9:14

--- src/components/DailyInfoCard.js
+++ src/components/DailyInfoCard.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
 import style from "../style/dailyInfoCard.module.css";
-import data from '../data/dailyCard.json';
+import data from "../data/dailyCard.json";
 
 export default function dailyInfoCard() {
-    return (
-        <h2> I'm daily info card </h2>
-    );
+  return (
+    <h2 className={style.dailyCard}>
+      {data.map((item) => {
+        return (
+          <div>
+            <span className="">{item.time}</span>
+            <div>
+                <span>{item.title}</span>
+                <p>{item.description}</p>
+            </div>
+          </div>
+        );
+      })}
+    </h2>
+  );
 }

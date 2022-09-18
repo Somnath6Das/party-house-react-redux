099ed5b910a50de0ef556a1c3d1e422efac7bf03
Somnath6Das <98904596+Somnath6Das@users.noreply.github.com>
2022-09-18 12:42:51 +0530

    bottom-button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Header from '../components/Header'
 import DailyInfoCard from '../components/DailyInfoCard';
 import style from '../style/home.module.css'
+import RoomInfoCard from '../components/RoomInfoCard';
+import { AiOutlinePlus } from 'react-icons/ai';
+import { BsGrid3X3Gap } from "react-icons/bs";
+
 
 export default function Home() {
     return (
@@ -10,6 +14,11 @@ export default function Home() {
         <Header/>
         <div className={style.home_container}>
         <DailyInfoCard/>
+        <RoomInfoCard/>
+        </div>
+        <div className={style.action_btn}>
+            <button><AiOutlinePlus className='mr-2'/></button>
+            <button><BsGrid3X3Gap/></button>
         </div>
         </>
 

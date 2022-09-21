03c64a73c4e17e414dea4984b438bb6542ef1b5b
Somnath6Das <98904596+Somnath6Das@users.noreply.github.com>
2022-09-22 01:12:50 +0530

    ok

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React,{ useState } from "react";
 import Header from "../components/Header";
 import DailyInfoCard from "../components/DailyInfoCard";
 import style from "../style/home.module.css";
 import RoomInfoCard from "../components/RoomInfoCard";
 import { AiOutlinePlus } from "react-icons/ai";
 import { BsGrid3X3Gap } from "react-icons/bs";
+import data from "../data/roomCard.json"
+
+
 
 export default function Home() {
+              // 9:48:52
   return (
     <>
       <Header />
